12d4beaecf042ff7e7c41030a829aa2011a773f0
qcgm1978 <qcgm197874@gmail.com>
2020-12-05 16:22:04 +0800

    calculus

--- sympy/testing/tests/test_calculus.py
+++ sympy/testing/tests/test_calculus.py
@@ -1,10 +1,16 @@
 import unittest
+from sympy import *
+
 class TDD_TEST_CALCULUS(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.foo = 1
+        x,y,z=symbols('x y z')
+        cls.x = x
+        cls.y = y
+        cls.z=z
+        init_printing(use_unicode=True)
     def test_test_calculus(self):
-        self.assertEqual(self.foo,1)
+        self.assertIsInstance(self.x,Symbol)
 if __name__ == '__main__':
     unittest.main()
                 
